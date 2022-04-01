0fa5b38b9195adfe6a11d5ff9ccbbe23b54ffbcb
Gabriel Moreira <moreira.gabrielsoares@gmail.com>
2022-04-01 14:20:38 -0300

    Envio do pedido implementado

--- scripts.js
+++ scripts.js
@@ -8,3 +8,19 @@ function ativarSelecao (elementoClicado) {
 
     elementoClicado.classList.add("borda-item-selecionado");
 }
+
+function criarLinkComMensagemPronta () {
+    nomeCliente = prompt("Qual o seu nome?");
+    enderecoCliente = prompt("Qual o seu endereço?");
+    let pratoPrincipalSelecionado = "Frango Ying Yang";
+    let bebidaSelecionada = "Coquinha gelada";
+    let sobremesaSelecionada = "Pudim";
+    let valorPratoPrincipal = 10.90;
+    let valorBebida = 10.90;
+    let valorSobremesa = 10.90;
+    let valorFinal = (valorPratoPrincipal + valorBebida + valorSobremesa).toFixed(2);
+    let mensagemPronta = `Olá, gostaria de fazer o pedido:\n- Prato: ${pratoPrincipalSelecionado}\n- Bebida: ${bebidaSelecionada}\n- Sobremesa: ${sobremesaSelecionada}\nTotal: R$ ${valorFinal}\n\nNome: ${nomeCliente}\nEndereço: ${enderecoCliente}`;
+    let encoded = encodeURIComponent(mensagemPronta);
+    let linkFinal = `https://wa.me/5521992992511?text=${encoded}`
+    window.location.assign(linkFinal);
+}
